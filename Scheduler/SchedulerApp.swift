dcd3b0f63f58daa8aec31f48a925f83c847ff9ec
NotArtwork <AntArce3@gmail.com>
2025-01-08 11:28:59 -0500

    Initial Commit

---
+++ Scheduler/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Scheduler
+//
+//  Created by Antonio Arce on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
