e3cb1d40f4e93d24c894516db35b4c22bc17de76
NotArtwork <AntArce3@gmail.com>
2025-01-08 13:03:18 -0500

    added image

--- Scheduler/ContentView.swift
+++ Scheduler/ContentView.swift
@@ -13,15 +13,32 @@ struct ContentView: View {
     @State private var wrongUsername = 0
     @State private var wrongPassword = 0
     @State private var showingLoginScreen = false
+    
+    
     var body: some View {
-        
-        VStack {
-            
-        }
-        .padding()
+        NavigationView{
+            VStack {
+            Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: UIScreen.main.bounds.width * 0.8,
+                           maxHeight: UIScreen.main.bounds.height * 0.3)
+                    .padding(5)
+            Spacer()
+            }
+        }.navigationBarHidden(true)
     }
+    
+
 }
 
 #Preview {
     ContentView()
 }
+
+//struct ContentView_Preview:
+//    PreviewProvider {
+//    static var previews: some View{
+//        ContentView()
+//    }
+//}
