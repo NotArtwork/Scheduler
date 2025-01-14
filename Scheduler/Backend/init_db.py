ca3590c58d4f9808ba6b43740f2ecc6835259f0d
Alina Pisarenko <78824916+AlinaPisarenko@users.noreply.github.com>
2025-01-14 13:50:07 -0500

    somehow fixed database

--- Scheduler/Backend/init_db.py
+++ Scheduler/Backend/init_db.py
@@ -3,6 +3,6 @@ from models import db
 
 # Initialize the database
 
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!")
+# with app.app_context():
+#     db.create_all()
+#     print("Database tables created successfully!")
